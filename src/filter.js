4a8638c5816bed2a81889a113c78a5e4b1966786
Mikhail-Strygin <mixa123_77@bk.ru>
2024-12-20 18:50:11 +0300

    Рефакторинг filter

--- src/filter.js
+++ src/filter.js
@@ -1,30 +1,30 @@
-import { renderTasks, clearTasks } from "./display.js"
+import { renderTasks, clearTasks } from "./display.js";
 
-const select = document.querySelector('#filter-assignee')
-
+const select = document.querySelector("#filter-assignee");
+let handlerFilter = () => {};
 export const filter = (tasks) => {
-  const handlerFilter = async (event) => {
+  handlerFilter = async (event) => {
     clearTasks();
 
     const user_id = event.target.value;
 
-    if (!isNaN(user_id)) { // Если число, значит айди; отрисовываем по нему
+    if (!isNaN(user_id)) {
+      // Если число, значит айди; отрисовываем по нему
       await renderTasksByUserId(user_id, tasks);
     } else {
       await renderTasks(tasks);
     }
-  }
+  };
 
-  select.removeEventListener("change", handlerFilter)
-  select.addEventListener('change', handlerFilter)
-}
+  select.removeEventListener("change", handlerFilter);
+  select.addEventListener("change", handlerFilter);
+};
 
-
-const renderTasksByUserId = async(user_id, tasks) => {
-    const filteredTasks = tasks.filter((task) => {
-        if (task.assignedTo == user_id) {
-            return task
-        }
-    });
-    await renderTasks(filteredTasks);
-}
+const renderTasksByUserId = async (user_id, tasks) => {
+  const filteredTasks = tasks.filter((task) => {
+    if (task.assignedTo == user_id) {
+      return task;
+    }
+  });
+  await renderTasks(filteredTasks);
+};
